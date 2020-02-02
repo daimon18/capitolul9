db0adbd30f5c89c069bde766021282b7755530b2
Nicolae <55551784+daimon18@users.noreply.github.com>
2020-02-02 18:16:07 +0200

    conferinta

--- conferintaa/conferinta/main.cpp
+++ conferintaa/conferinta/main.cpp
@@ -23,6 +23,7 @@ using namespace std;
     cout<<"2. Francez"<<endl;
     cout<<"3. German"<<endl;
     cout<<"4. Alta"<<endl;
+    cout<<"Introduceti nationalitatea:"<<endl;
     unsigned short a;
     cin>>a;
     switch (a)
@@ -30,7 +31,7 @@ using namespace std;
         case 1:{Participant* concurent=new Roman();return concurent; break;}
         case 2:{Participant* concurent=new Francez();return concurent; break;}
         case 3:{Participant* concurent=new German();return concurent; break;}
-        case 4:{Participant* concurent=new Participant();return concurent; break;}
+        case 4:{Participant* Altconcurent=new Participant();return Altconcurent; break;}
     }
 
 }
@@ -39,37 +40,43 @@ int main()
 
     vector <Participant*> pntParticipanti;
         Meniu();
+        cout<<"Introduceti:"<<endl;
             unsigned short b;
             cin>>b;
+                if((b<1)&&(b>3))
+                {
+                    cout<<"Reintroduceti optiunea:"<<endl;
+                    cin>>b;
+                }
+                while(b!=0)
+                {
                 unsigned short roman=0,francez=0,german=0,alta=0;
-        while(b!=0){
         if (b!=0&&b<=3)
         {
             if(b==1){;pntParticipanti.push_back(CreeazaParticipant());}
-            else if (b==2)
-            {system("CLS");
-            for(unsigned int idx=0; idx<pntParticipanti.size();++idx)
-                switch (pntParticipanti[idx]->Vorbeste())
-                {
+                else if (b==2)
+                {system("CLS");
+                for(unsigned int idx=0; idx<pntParticipanti.size();++idx)
+                    switch (pntParticipanti[idx]->Vorbeste())
+                    {
                     case 1: roman++;break;
                     case 2: german++;break;
                     case 3: francez++;break;
                     case 4: alta++;break;
-                }
+                    }
                     if (roman>0||german>0||francez>0||alta>0) cout<<"Participanti inscrisi pana acum: "<<endl;
                          if (roman>0||german>0||francez>0||alta>0) {cout<<roman<<" Romani"<<endl;
                                                               cout<<german<<" Germani"<<endl;
                                                               cout<<francez<<" Francezi"<<endl;
                                                               cout<<alta<<" Altii"<<endl;}
                         system("pause");
-            }else if(b==3)
-            {
+            }
             system("CLS");
-            };
             Meniu();
             cout<<"Introduceti"<<endl;
              cin>>b;
-        }else cout<<"Introduceti din nou."<<endl;}
+        }}
         pntParticipanti.clear();
     return 0;
+
 }
